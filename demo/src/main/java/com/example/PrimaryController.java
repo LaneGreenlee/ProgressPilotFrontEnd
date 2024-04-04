15721b53adbfd4d0da0bf920f445902c68698398
Hani Rehmani <hanirehmani03@gmail.com>
2024-04-04 14:16:48 -0400

    Changed first page to match log in baselin

--- demo/src/main/java/com/example/PrimaryController.java
+++ demo/src/main/java/com/example/PrimaryController.java
@@ -6,6 +6,22 @@ import javafx.fxml.FXML;
 public class PrimaryController {
 
     @FXML
+    private void signInAsUser() throws IOException {
+        // TODO: Add your logic here for user sign-in
+    }
+    
+    @FXML
+    private void signInAsAdvisor() throws IOException {
+        // TODO: Add your logic here for advisor sign-in
+    }
+    
+    @FXML
+    private void signUp() throws IOException {
+        // TODO: Add your logic here for user sign-up
+    }
+    
+    // You can keep the switchToSecondary method if you still need it for other navigation purposes
+    @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
