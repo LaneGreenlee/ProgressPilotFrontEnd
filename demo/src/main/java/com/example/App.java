b5a4ad1a31b19fdff4625a686e3fbce3d2aca81a
LaneGreenlee <lanegreenlee9@gmail.com>
2024-04-09 13:43:27 -0400

    -

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("Untitled"), 640, 480);
+        scene = new Scene(loadFXML("login_page"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
