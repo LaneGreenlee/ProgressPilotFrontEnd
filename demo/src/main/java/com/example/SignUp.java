a39c75f2b16ed0db2181c0d9f7daea04bfcff5a7
Foster Nicodin <fosternicodin@Fosters-MacBook-Pro.local>
2024-04-10 09:09:47 -0400

    Finished signup page front end

--- demo/src/main/java/com/example/SignUp.java
+++ demo/src/main/java/com/example/SignUp.java
@@ -8,6 +8,7 @@ import javafx.scene.text.Text;
 import javafx.scene.text.Font;
 
 public class SignUp {
+
     @FXML
     private Rectangle background;
 
@@ -21,20 +22,43 @@ public class SignUp {
     private Text uscIdText;
 
     @FXML
+    private Text usernameText;
+
+    @FXML
+    private Text passwordText;
+
+    @FXML
+    private Text iAmAText;
+
+    @FXML
+    private Text signupText;
+
+    @FXML
     public void initialize() {
         // Calculate text size based on rectangle width
         double rectangleWidth = background.getWidth();
-        double textSize = rectangleWidth / 30; // Adjust this value to your preference
+        double textSize = rectangleWidth / 40; // Adjust this value to your preference
 
-        // Set font size for Text elements
-        Font font = Font.font(textSize);
+        // Set font size for each Text element
+        Font font = new Font(textSize);
+        Font signUpFont = new Font(50);
         firstNameText.setFont(font);
         lastNameText.setFont(font);
         uscIdText.setFont(font);
+        usernameText.setFont(font);
+        passwordText.setFont(font);
+        iAmAText.setFont(font);
+        signupText.setFont(signUpFont);
     }
 
+
     @FXML
-    private void SignUp() throws IOException {
-        // TODO: Add your logic here for user sign-in
+    private void signUp() throws IOException {
+        switchToLogIn();
+        // TODO: Add your logic here for user sign-up
+    }
+
+    private void switchToLogIn() throws IOException {
+        App.setRoot("login_page");
     }
 }
