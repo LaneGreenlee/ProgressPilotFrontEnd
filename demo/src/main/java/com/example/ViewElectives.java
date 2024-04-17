82bd01f90654c2a6d16c55f5d068c596d7d1967b
Foster Nicodin <fosternicodin@Fosters-MacBook-Pro.local>
2024-04-17 10:57:31 -0400

    Add class works and replace GFL Elective works

--- demo/src/main/java/com/example/ViewElectives.java
+++ demo/src/main/java/com/example/ViewElectives.java
@@ -2,13 +2,31 @@ package com.example;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import lib.Course;
-
 import java.io.IOException;
 import java.util.ArrayList;
+import javafx.scene.layout.VBox;
+import lib.CourseAttributes;
+import lib.Student;
 
 public class ViewElectives extends App{
+
+    @FXML
+    private Button addCourseButton;
+
+    @FXML
+    private Text promtNumber;
+
+    @FXML
+    private Text promtCode;
+
+    @FXML
+    private TextField courseCode;
+
+    @FXML
+    private TextField courseNumber;
 
     @FXML
     private Button GSS;
@@ -36,6 +54,12 @@ public class ViewElectives extends App{
 
     @FXML
     private void gssSelect() {
+        courseCode.setVisible(false);
+        courseNumber.setVisible(false);
+        promtCode.setVisible(false);
+        promtNumber.setVisible(false);
+        addCourseButton.setVisible(false);
+        elecClass.setVisible(true);
         StringBuilder stringBuilder = new StringBuilder();
         elecType.setText("\t     GSS Electives");
         ArrayList<Course> gssCourses = facade.courseList.getGSS();
@@ -46,6 +70,12 @@ public class ViewElectives extends App{
 
     @FXML
     private void ghsSelect() {
+        courseCode.setVisible(false);
+        courseNumber.setVisible(false);
+        promtCode.setVisible(false);
+        promtNumber.setVisible(false);
+        addCourseButton.setVisible(false);
+        elecClass.setVisible(true);
         StringBuilder stringBuilder = new StringBuilder();
         elecType.setText("\t     GHS Electives");
         ArrayList<Course> ghsCourses = facade.courseList.getGHS();
@@ -56,6 +86,12 @@ public class ViewElectives extends App{
 
     @FXML
     private void infSelect() {
+        courseCode.setVisible(false);
+        courseNumber.setVisible(false);
+        promtCode.setVisible(false);
+        promtNumber.setVisible(false);
+        addCourseButton.setVisible(false);
+        elecClass.setVisible(true);
         StringBuilder stringBuilder = new StringBuilder();
         elecType.setText("\t     INF Electives");
         ArrayList<Course> infCourses = facade.courseList.getINF();
@@ -66,6 +102,12 @@ public class ViewElectives extends App{
 
     @FXML
     private void aiuSelect() {
+        courseCode.setVisible(false);
+        courseNumber.setVisible(false);
+        promtCode.setVisible(false);
+        promtNumber.setVisible(false);
+        addCourseButton.setVisible(false);
+        elecClass.setVisible(true);
         StringBuilder stringBuilder = new StringBuilder();
         elecType.setText("\t     AIU Electives");
         ArrayList<Course> aiuCourses = facade.courseList.getAIU();
@@ -76,6 +118,12 @@ public class ViewElectives extends App{
 
     @FXML
     private void cmsSelect() {
+        courseCode.setVisible(false);
+        courseNumber.setVisible(false);
+        promtCode.setVisible(false);
+        promtNumber.setVisible(false);
+        addCourseButton.setVisible(false);
+        elecClass.setVisible(true);
         StringBuilder stringBuilder = new StringBuilder();
         elecType.setText("\t     CMS Electives");
         ArrayList<Course> cmsCourses = facade.courseList.getCMS();
@@ -86,6 +134,12 @@ public class ViewElectives extends App{
 
     @FXML
     private void gflSelect() {
+        courseCode.setVisible(false);
+        courseNumber.setVisible(false);
+        promtCode.setVisible(false);
+        promtNumber.setVisible(false);
+        addCourseButton.setVisible(false);
+        elecClass.setVisible(true);
         StringBuilder stringBuilder = new StringBuilder();
         elecType.setText("\t     GFL Electives");
         ArrayList<Course> gflCourses = facade.courseList.getGFL();
@@ -95,14 +149,63 @@ public class ViewElectives extends App{
     }
 
     @FXML
+    private void addACourse() {
+        elecType.setText("\t     Add A Course");
+        courseCode.setVisible(true);
+        courseNumber.setVisible(true);
+        promtCode.setVisible(true);
+        promtNumber.setVisible(true);
+        addCourseButton.setVisible(true);
+        elecClass.setVisible(false);
+    }
+
+    @FXML
+    private void addCourseInput() {
+        String code = courseCode.getText().toUpperCase();
+        String number = courseNumber.getText();
+        System.out.println(code + number);
+        int gflIndex = -1;
+        for (int i = 0; i < StudentLogin.currentStudent.getMajor().courses.size(); i++) {
+            Course course = StudentLogin.currentStudent.getMajor().courses.get(i);
+            if (course.getCourseCode().equals("GFL Elective")) {
+                gflIndex = i;
+                break;
+            }
+        }
+        if (gflIndex != -1) {
+            for (Course course : facade.courseList.Courses) {
+                if (code.equals(course.getCourseCode()) && number.equals(course.getCourseNumber())) {
+                    if (Course.isGFL(course)) {
+                        // If the course is found, replace the GFL elective with the new course
+                        StudentLogin.currentStudent.getMajor().courses.set(gflIndex, course);
+                        System.out.println("Course successfully added and replaced the GFL elective.");
+                        return; // Exit the method once the course is added
+                    } else {
+                        StudentLogin.currentStudent.getMajor().courses.add(course);
+                        System.out.println("Course successfully added");
+                    }
+                }
+            }
+            System.out.println("Course not found.");
+        } else {
+            for (Course course : facade.courseList.Courses) {
+                if (code.equals(course.getCourseCode()) && number.equals(course.getCourseNumber())) {
+                    StudentLogin.currentStudent.getMajor().courses.add(course);
+                    System.out.println("Course successfully added");
+
+                }
+            }
+        }
+    }
+
+    @FXML
     private void returnToOverview() throws IOException {
-        switchtoOverview();;
+        switchToOverview();
         // TODO: Add your logic here for user sign-up
     }
 
     @FXML
-    private void switchtoOverview() throws IOException {
+    private void switchToOverview() throws IOException {
         App.setRoot("studentOverview");
-
     }
 }
